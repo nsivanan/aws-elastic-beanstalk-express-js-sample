b32c25c8729355b2d1c3f0b369eec417ac4e140d
nsivanan <nithin.cs@aeriestechnology.com>
2025-02-08 13:12:18 +0530

    adding more options

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing the CICD! testing teh review option'));
+app.get('/', (req, res) => res.send('Testing the CICD! testing teh review option. Adding more options here'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
